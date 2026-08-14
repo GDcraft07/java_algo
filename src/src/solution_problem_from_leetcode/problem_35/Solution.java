127cf5202cf80adec872f616651f1dc6f7a812af
gdcraft07 <iklass987654321@gmail.com>
2026-08-14 23:17:47 +0300

    solve problem 35

---
+++ src/src/solution_problem_from_leetcode/problem_35/Solution.java
@@ -0,0 +1,27 @@
+package solution_problem_from_leetcode.problem_35;
+
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int start = 0;
+        int end = nums.length - 1;
+
+        while (start <= end) {
+            int middle = (start + end) / 2;
+
+            if (nums[middle] == target) {
+                return middle;
+            }
+
+            else {
+                if (nums[middle] > target) {
+                    end = middle - 1;
+                }
+                else {
+                    start = middle + 1;
+                }
+            }
+        }
+
+        return start;
+    }
+}
---
+++ src/src/solution_problem_from_leetcode/problem_35/Test.java
@@ -0,0 +1,11 @@
+package solution_problem_from_leetcode.problem_35;
+
+public class Test {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        int[] nums = new int[]{1,3,5,6};
+
+        System.out.print(solution.searchInsert(nums, 5));
+    }
+}
